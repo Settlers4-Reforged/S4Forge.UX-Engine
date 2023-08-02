8c633f91792a8a3dec66760fee049a16ed676860
WizzardMaker <wizzardmaker@gmail.com>
2023-08-02 03:35:22 +0200

    added input manager interface and mouse interaction endpoints

---
+++ Input/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using Forge.UX.Native;
+
+using System;
+using System.Numerics;
+
+namespace Forge.UX.Input {
+    public static class InputManager {
+        //Mouse:
+        public static bool IsMouseInRectangle(Vector4 rect) {
+            throw new NotImplementedException();
+        }
+
+        public static float GetMouseScroll() {
+            throw new NotImplementedException();
+        }
+
+        //Key related input:
+        public static bool IsKeyDown(Keys key) {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsKeyUp(Keys key) {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsKeyHeld(Keys key) {
+            throw new NotImplementedException();
+        }
+
+        //Original Game Input Blocking:
+        public static void ClearBlockFlags() {
+            throw new NotImplementedException();
+        }
+
+        public static void BlockInput(EventBlockFlags flags) {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Flags]
+    public enum EventBlockFlags : int {
+        None = 0b0000_0000,
+        MouseClick = 0b0000_0001,
+        MouseWheel = 0b0000_0010,
+        Mouse = 0b0000_0100,
+        Keyboard = 0b0000_1000,
+        All = 0b1111_1111
+    }
+}
---
+++ Native/Mouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Forge.UX.Native {
+    internal static class Mouse {
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(ref Vector2 lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern bool ScreenToClient(IntPtr hWnd, ref Vector2 lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hWnd, out Vector4 lpRect);
+    }
+}
