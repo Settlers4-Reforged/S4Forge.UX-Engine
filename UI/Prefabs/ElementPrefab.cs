c25345f4290248ee052eb4c1f3586612652ac4bf
WizzardMaker <wizzardmaker@gmail.com>
2023-08-09 23:09:04 +0200

    added element prefab base

---
+++ UI/Prefabs/ElementPrefab.cs
@@ -0,0 +1,57 @@
+﻿using Forge.UX.UI.Elements;
+using Forge.UX.UI.Prefabs.Properties;
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Forge.UX.UI.Prefabs {
+    public abstract class ElementPrefab : IPrefab {
+        public abstract string Name { get; }
+        public abstract UIElement Create();
+
+        #region Properties
+
+        public RelativeProperty X = new(nameof(X), "X position of the element");
+        public RelativeProperty Y = new(nameof(Y), "Y position of the element");
+
+        public RelativeProperty Width = new(nameof(Width), "Width of the element");
+        public RelativeProperty Height = new(nameof(Height), "Height of the element");
+
+        public Property<bool> PositionAbsolute = new(nameof(PositionAbsolute), "Whether the position is relative to group space (false) or screen space (true)");
+
+        public Property<bool> IgnoreMouse = new(nameof(IgnoreMouse), "Whether to ignore mouse inputs - affects children");
+
+        public Property<int> ZIndex = new(nameof(ZIndex), "Z-Index of element");
+
+        protected void ApplyPropertyValues(UIElement element) {
+            UIElement.PositioningMode posMode = PositionAbsolute.Value ? UIElement.PositioningMode.Absolute : UIElement.PositioningMode.Normal;
+
+            element.PositionMode = (
+                posMode | (X.IsRelative ? UIElement.PositioningMode.Relative : UIElement.PositioningMode.Normal),
+                posMode | (Y.IsRelative ? UIElement.PositioningMode.Relative : UIElement.PositioningMode.Normal)
+            );
+
+            element.SizeMode = (
+                posMode | (Width.IsRelative ? UIElement.PositioningMode.Relative : UIElement.PositioningMode.Normal),
+                posMode | (Height.IsRelative ? UIElement.PositioningMode.Relative : UIElement.PositioningMode.Normal)
+            );
+
+            element.Position = new Vector2(X.Value, Y.Value);
+            element.Size = new Vector2(Width.Value, Height.Value);
+
+            element.IgnoresMouse = IgnoreMouse.Value;
+
+            element.ZIndex = ZIndex.Value;
+        }
+
+        #endregion
+
+        public IEnumerable<IProperty> GetProperties() {
+            List<IProperty> properties = new();
+
+            return properties;
+        }
+    }
+}
