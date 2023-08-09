a9c511147ec11bf22f4c27311c561626d54e2660
WizzardMaker <wizzardmaker@gmail.com>
2023-08-09 22:40:44 +0200

    added error handling interface

---
+++ Interfaces/IErrorHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forge.UX.Interfaces {
+    public interface IErrorHandler {
+        string? GetLastError();
+    }
+}
