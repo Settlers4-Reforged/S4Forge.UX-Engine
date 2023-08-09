3cf3c36e05059d38f3fe87c9373df928b5da46a2
WizzardMaker <wizzardmaker@gmail.com>
2023-08-09 23:01:58 +0200

    added layout refresh to UI Group

--- UI/Elements/Grouping/UIGroup.cs
+++ UI/Elements/Grouping/UIGroup.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using Forge.UX.UI.Elements.Grouping.Layout;
+
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Forge.UX.UI.Elements.Grouping {
-    public class UIGroup : UIElement {
+    public class UIGroup : UIElement, IUILayout {
         public List<UIElement> Elements { get; set; }
 
         public bool ClipContent { get; set; } = false;
@@ -15,6 +17,27 @@ namespace Forge.UX.UI.Elements.Grouping {
 
         public IEnumerable<UIElement> GetSortedElements() {
             return Elements.OrderBy(element => element.ZIndex);
+        }
+
+        public UIElement? GetElement(string id) {
+            foreach (var element in Elements) {
+                if (element.Id == id) {
+                    return element;
+                }
+
+                if (element is UIGroup g) {
+                    return g.GetElement(id);
+                }
+            }
+
+            return null;
+        }
+
+
+        public virtual void Relayout() {
+            foreach (IUILayout element in Elements.OfType<IUILayout>()) {
+                element.Relayout();
+            }
         }
     }
 }
