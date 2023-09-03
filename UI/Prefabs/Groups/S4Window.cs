a0a56fd5222fe73f19e27ad754f1c6ea0eaad600
WizzardMaker <wizzardmaker@gmail.com>
2023-09-04 01:27:23 +0200

    added s4window prefab

---
+++ UI/Prefabs/Groups/S4Window.cs
@@ -0,0 +1,21 @@
+﻿using Forge.UX.Rendering.Texture;
+using Forge.UX.UI.Components;
+using Forge.UX.UI.Elements;
+using Forge.UX.UI.Elements.Grouping.Display;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forge.UX.UI.Prefabs.Groups {
+    public class S4Window : GroupPrefab {
+        public override string Name => "S4Window";
+        public override string Description => "A generic Window in the style of a S4 pop-out";
+
+        public override UIElement Instantiate() {
+            UIWindow element = new UIWindow(new NineSliceTextureComponent(UXEngine.TCM.Get((int)TextureCollectionMap.ForgeUI, (int)ForgeTextureMap.Window), 0, 0, 0, 0));
+            this.ApplyPropertyValues(element);
+            return element;
+        }
+    }
+}
