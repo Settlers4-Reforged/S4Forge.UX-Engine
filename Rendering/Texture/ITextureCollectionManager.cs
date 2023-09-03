7f8557fb33211d9bd09397ee5d6acc651669f5c7
WizzardMaker <wizzardmaker@gmail.com>
2023-09-04 01:24:25 +0200

    updated texture collection manager api

--- Rendering/Texture/ITextureCollectionManager.cs
+++ Rendering/Texture/ITextureCollectionManager.cs
@@ -2,8 +2,6 @@
 
 namespace Forge.UX.Rendering.Texture {
     public interface ITextureCollectionManager {
-        void AddCollection(int id, ITextureCollection collection);
-        void RemoveCollection(int id);
         ITextureCollection GetCollection(int id);
         ITexture Get(int col, int id);
     }
