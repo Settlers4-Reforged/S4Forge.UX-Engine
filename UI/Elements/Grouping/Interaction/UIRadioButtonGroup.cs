8935a37c6efd4565e2f6b94628c67d13a27602cf
WizzardMaker <wizzardmaker@gmail.com>
2023-10-25 19:39:55 +0200

    added radio button elements

---
+++ UI/Elements/Grouping/Interaction/UIRadioButtonGroup.cs
@@ -0,0 +1,56 @@
+﻿using Forge.UX.UI.Elements.Interaction;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+
+namespace Forge.UX.UI.Elements.Grouping.Interaction {
+    public class UIRadioButtonGroup<T> : UIGroup, IUIBindable<T>, IUIValueObserver<T> {
+        public UIRadioButtonGroup(string groupLinkId) {
+            GroupLinkId = groupLinkId;
+            Elements ??= new SceneTree();
+
+            Elements.CollectionChanged += SceneTreeChanged;
+        }
+
+        private void SceneTreeChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            // Update all radio buttons with the same link id
+            foreach (UIElement element in Elements.GetAllElementsInTree()) {
+                if (element is UIRadioButton<T> rbX) {
+                    rbX.OnValueChange = (target, check) => {
+                        UIRadioButton<T>? rb = (UIRadioButton<T>)target;
+
+                        // Ignore unchecks
+                        if (!check) return;
+
+                        foreach (UIElement e in Elements.GetAllElementsInTree()) {
+                            // Uncheck all other radio buttons with the same link id
+                            if (e is UIRadioButton<T> rb2 && rb2.LinkId == rb.LinkId && rb2 != rb) {
+                                rb2.IsChecked = false;
+                            }
+                        }
+                    };
+                }
+            }
+        }
+
+        public override bool IsTransparent => true;
+
+        public string GroupLinkId { get; set; }
+
+
+        public UIRadioButton<T>? SelectedRadioButton => Elements.GetAllElementsInTree<UIRadioButton<T>>().FirstOrDefault(element => element is { IsChecked: true });
+
+        public T? SelectedValue => SelectedRadioButton != null ? SelectedRadioButton.Value : default;
+
+        public override void Input(SceneGraphState state) {
+            base.Input(state);
+        }
+
+        public Func<UIElement, T>? BindingGetValue { get; set; }
+        public Action<UIElement, T>? OnValueChange { get; set; }
+    }
+}
--- UI/Elements/Interaction/UIRadioButton.cs
+++ UI/Elements/Interaction/UIRadioButton.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Forge.UX.UI.Elements.Interaction {
-    public class UIRadioButton : UICheckbox {
+    public class UIRadioButton<T> : UICheckbox {
         public string LinkId { get; set; }
+        public T Value { get; set; }
 
-        public UIRadioButton(string linkId) {
+        public UIRadioButton(string linkId, T value) {
             LinkId = linkId;
+            Value = value;
+        }
+
+        public override void OnMouseClickUp(int mb) {
+            // Don't allow unchecking
+            if (IsChecked) return;
+
+            IsChecked = true;
+            OnInteract?.Invoke(this);
         }
     }
 }
