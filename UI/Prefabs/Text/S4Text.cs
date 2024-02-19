f747a7c66bb66e38ad98a56851d4201f0aa12cc8
WizzardMaker <wizzardmaker@gmail.com>
2024-02-19 19:46:37 +0100

    added generic text prefab

---
+++ UI/Prefabs/Text/S4Text.cs
@@ -0,0 +1,16 @@
+﻿using DasMulli.DataBuilderGenerator;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.UX.UI.Prefabs.Text {
+
+    [GenerateDataBuilder]
+    public class S4Text : BaseText {
+        public override string Name => "S4Text";
+        public override string Description => "A generic text element";
+    }
+}
