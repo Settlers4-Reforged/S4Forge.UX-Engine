fdcac1e066879a12ab95445d8ffe12f0aad90cca
WizzardMaker <wizzardmaker@gmail.com>
2024-02-19 19:44:55 +0100

    added test helper class for dependencies

---
+++ Testing/UXTestDependencies.cs
@@ -0,0 +1,25 @@
+﻿using DryIoc;
+
+using Forge.Config;
+using Forge.UX.Input;
+using Forge.UX.UI;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.UX.Testing {
+    public static class UXTestDependencies {
+        public static void AddTestDependencies() {
+            DI.Dependencies.RegisterInstance<IInputManager>(new TestInputManager());
+            DI.Dependencies.RegisterInstance(new UIEngine());
+
+            DI.Dependencies.RegisterMany<TextureCollectionManagerMock>();
+            DI.Dependencies.RegisterMany<TextureCollectionManagerMock.TextureCollectionMock>();
+
+            DI.Resolve<SceneManager>().Init();
+        }
+    }
+}
