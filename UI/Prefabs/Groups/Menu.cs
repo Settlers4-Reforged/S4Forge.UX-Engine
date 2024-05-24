713e5918641c80d48a84b4c014255da88628e1c0
WizzardMaker <wizzardmaker@gmail.com>
2024-05-24 19:38:07 +0200

    added menu prefab

---
+++ UI/Prefabs/Groups/Menu.cs
@@ -0,0 +1,28 @@
+﻿using DasMulli.DataBuilderGenerator;
+
+using Forge.UX.UI.Elements;
+using Forge.UX.UI.Elements.Grouping;
+using Forge.UX.UI.Elements.Grouping.Display;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.UX.UI.Prefabs.Groups {
+
+    [GenerateDataBuilder]
+    public class Menu : GroupPrefab {
+        public override string Name => "Menu";
+        public override string Description => "A fullscreen menu group - often attached to a S4 menu";
+        public override UIElement Instantiate() {
+            UIMenu group = new UIMenu();
+            this.ApplyPropertyValues(group);
+
+            InstantiateChildren(group);
+
+            return group;
+        }
+    }
+}
