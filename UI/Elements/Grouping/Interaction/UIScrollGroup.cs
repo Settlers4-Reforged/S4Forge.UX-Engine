abfefb16d417d243826122e639926b5271cb05ac
WizzardMaker <wizzardmaker@gmail.com>
2024-05-24 19:38:34 +0200

    added UIScrollGroup element placeholder

---
+++ UI/Elements/Grouping/Interaction/UIScrollGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.UX.UI.Elements.Grouping.Interaction {
+    public class UIScrollGroup : UIGroup {
+        /// <summary>
+        /// The maximum size of the scroll area. Anything outside of this area will be clipped and be scrollable to.
+        /// </summary>
+        public Vector2 MaxSize { get; set; }
+
+        /// <summary>
+        /// The current scroll offset of the scroll area, when content is larger than the max size.
+        /// </summary>
+        public Vector2 ScrollPosition { get; protected set; }
+
+        /// <summary>
+        /// Whether to provide a scrollbar for the scroll area, or if the scrolling should be initiated by code.
+        /// </summary>
+        /// <remarks>
+        /// This also toggles the ability to scroll with the mouse scroll wheel.
+        /// </remarks>
+        public bool ManualScrolling { get; set; } = false;
+
+        public override void Input(SceneGraphState state) {
+            base.Input(state);
+
+            //TODO: Implement scrolling
+        }
+    }
+}
